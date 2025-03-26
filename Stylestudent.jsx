7cc2348b2ed6f358a18e02ed883404474f7b9998
gowtham1194 <gowtham.s.ece.2022@snsce.ac.in>
2025-03-26 09:32:52 +0530

    Add files via upload

---
+++ Stylestudent.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+const Stylestudent = () => {
+    const style = {
+        color: "blue", 
+        fontSize: "24px", 
+    };
+
+    const Name = "Gowtham";
+    return (
+        <h1 style={style}>Name: {Name}</h1> 
+    );
+};
+
+export default Stylestudent;
+
+
+
